244c62922d55a42be508a7a508115046518bcd4a
Neville Mbuyi <orman.neville@hotmail.com>
2016-12-27 15:18:37 -0600

    Init ECAMO web app

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl : 'app/app.template.html',
+  styleUrls : ['app/app.style.css']
+})
+export class AppComponent  { name = 'Angular'; }
