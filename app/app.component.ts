676586b245c368f449d8deba1261b0b2928e42f5
Orman <orman.neville@hotmail.com>
2018-08-27 06:54:36 -0500

    cleanup

--- app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  templateUrl : 'app/app.template.html',
-  styleUrls : ['app/app.style.css']
-})
-export class AppComponent  { name = 'Angular'; }
